c8c519213992dd901dbcb52c204c16d516ebf06d
yooow0t <yooow0t@protonmail.ch>
2017-11-08 11:41:29 +0900

    Updated main file
    Added procedure and variable define syntax as well as if statements

--- genghis.py
+++ genghis.py
@@ -5,24 +5,23 @@ def tokenize(string):
 
 def parse(tokens):
     expression = []
-    
+
     if tokens[0] == '(':
-        while tokens[0] != ')':
+        tokens.pop(0)
+
+    while tokens:
+        if tokens[0] == '(': 
+            expression.append(parse(tokens))
+
+        elif tokens[0] == ')':
+            tokens.pop(0)
+            return expression
+
+        else:
             expression.append(type_correct(tokens.pop(0)))
-            
-            if tokens[0] == '(':
-                expression.append(parse(tokens))
 
-            if tokens[0] == ')':
-                expression.append(tokens.pop(0))
-                return expression[1:][:-1]
-            
-
-    elif tokens[0] == ')':
-        raise SyntaxError("Expression begining with ')'")
-
-    else:
-        return tokens
+    return expression   
+        
 
 def type_correct(string):
     if '.' in string:
@@ -34,37 +33,102 @@ def type_correct(string):
         except: return string
 
 
-def environment():
+def initial_env():
     env = {'+' : op.add,
            '-' : op.sub,
            '*' : op.mul,
            '=' : op.eq,
            '/' : op.truediv,
+           '>' : op.gt,
+           '<' : op.lt,
+           '>=' : op.ge,
+           '=>' : op.le,
+           '!=' : op.ne,
+           'not' : op.__not__,
+           'and' : op.__and__,
+           'or' : op.__or__,
+           '#t' : True,
+           '#f' : False,
            'remainder' : op.mod,
            'expt' : op.pow,
-           'abs' : op.abs}
+           'abs' : op.abs,
+           'if' : 'if',
+           'define': 'define'}
 
     return env
 
 
-def evaluate(exp, env=environment()):
-    for i in range(len(exp)):
-        if isinstance(exp[i], list):
-            exp[i] = evaluate(exp[i], env)
+def evaluate(exp, env):
+    if exp[0] != 'define':
+        for i in range(len(exp)):
+            if isinstance(exp[i], list):
+                operator = exp[i][0]
+                arg1 = exp[i][1]
+                if operator != 'define' and not isinstance(arg1, list):
+                    exp[i] = evaluate(exp[i], env)[0]
 
+    exp_len = len(exp)
+                                
     def solve():
-        if len(exp) > 1:
+        if operator == 'if':
+            if exp[0]:
+                exp[2]
+                return exp[1]
+            else:
+                exp[1]
+                return exp[2]
+        
+        elif operator == 'define':
+            var = exp[0]
+            if isinstance(var, list):
+                name = var[0]
+                args = list(var[1:])
+                body = list(exp[1])
+                env[name] = lambda *x : function_eval(body, args, env, x)
+                return env[name]
+
+            elif isinstance(var, str):
+                env[var] = exp[1]
+                return exp[1]
+            else:
+                raise TypeError("numbers may not be redefined")
+
+        elif len(exp) > 1:
+            #This section here is not sensitive to anything but two arguments
             return operator(exp.pop(0), solve())
 
         else:
             return exp.pop(0)
-    
+
     operator = env[exp.pop(0)]
 
-    return solve()
+    if "lambda" in str(operator):
+        return operator(*exp), env
+
+    #Allows evaluation of variables and booleans
+    if operator != 'define':
+        for i in range(len(exp)): 
+            if isinstance(exp[i], str):
+                exp[i] = env[exp[i]]
+
+    return solve(), env
+
+
+def function_eval(body, args, env, x):
+    for i in range(len(args)):
+        for n in range(len(body)):
+            if body[n] == args[i]:
+                body[n] = x[i]
+
+    return evaluate(body, env)[0] 
 
 
 def repl():
+    user_env = {}
+    user_env.update(initial_env())
+    #for some reason when square is called it doesn't even reach here
+    user_env["square"] = lambda *x : function_eval(["*", "x", "x"], ["x"], user_env, x)
+
     while True:
         exp = input('\u03bb : ')
 
@@ -75,8 +139,8 @@ def repl():
             continue
 
         else:
-            print(evaluate(parse(tokenize(exp))))
-        
+            out, user_env = evaluate(parse(tokenize(exp)), user_env)
+            print(out)
 
 if __name__ == "__main__":
     print(repl())
